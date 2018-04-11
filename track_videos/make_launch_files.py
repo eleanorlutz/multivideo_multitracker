a4653bf367cb3064f9e06dd36f16b04b107a7bea
Eleanor Lutz <TabletopWhale@gmail.com>
2018-04-10 17:10:51 -0700

    edit default launch file to add large buffer size

--- track_videos/make_launch_files.py
+++ track_videos/make_launch_files.py
@@ -4,7 +4,7 @@ fnames = glob.glob(os.environ["VIDEO_SRC"]+'*')
 save_dir = os.environ["LAUNCH_FILES_SRC"]
 
 launch_text_before = '<launch>\n    <include file="$(find video_stream_opencv)/launch/camera.launch" >\n        <arg name="camera_name" value="camera" />\n        <arg name="video_stream_provider" value="'
-launch_text_after = '" />\n        <arg name="fps" value="30" />\n    </include>\n</launch>'
+launch_text_after = '" />\n        <arg name="fps" value="30" />\n        <arg name="buffer_queue_size" value="2000" />\n    </include>\n</launch>'
 
 for name in fnames:
 	name_short = name.split('/')[-1].split('.')[0]
